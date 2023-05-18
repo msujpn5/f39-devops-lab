320d5f4ee24ae8995283d1273c89c9489a0b05db
Rex Holmes <holmesrex515@gmail.com>
2023-05-19 06:59:43 +0900

    added rollbar

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const app = express()
 
 app.use(express.json())
 
+app.use(express.static(__dirname + '/public'))
+
 app.listen(4000, () => {
     console.log('up on 4000')
 })
+
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '19443cf46b0845708cec1d18633bce4a',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
